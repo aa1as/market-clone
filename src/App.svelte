61ebe99c7012d898db59f660d08dbd1d9d59a670
aa1as <sm13929881@gmail.com>
2024-05-11 05:10:10 +0900

    파이어베이스로 유저 인증까지 구현

--- src/App.svelte
+++ src/App.svelte
@@ -3,17 +3,48 @@
   import Login from "./pages/Login.svelte";
   import Signup from "./pages/Signup.svelte";
   import Write from "./pages/write.svelte";
-  import Router from "svelte-spa-router";
   import NotFound from "./pages/NotFound.svelte";
+  import Router from "svelte-spa-router";
+  import { user$ } from "./store";
   import "./css/style.css";
+  import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithCredential,
+  } from "firebase/auth";
+  import { onMount } from "svelte";
+  import Loading from "./pages/Loading.svelte";
+  import MyPage from "./pages/MyPage.svelte";
+
+  let isLoading = true;
+
+  const auth = getAuth;
+
+  const checkLogin = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return (isLoading = false);
+    const credential = GoogleAuthProvider.credential(null, token);
+    const result = await signInWithCredential(auth, credential);
+    const user = result.user;
+    user$.set(user);
+    isLoading = false;
+  };
 
   const routes = {
     "/": Main,
-    "/login": Login,
     "/signup": Signup,
-    write: Write,
+    "/write": Write,
+    "/my": MyPage,
     "*": NotFound,
   };
+
+  onMount(() => checkLogin());
 </script>
 
-<Router {routes} />
+{#if isLoading}
+  <Loading />
+{:else if !$user$}
+  <Login />
+{:else}
+  <Router {routes} />
+{/if}
--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,15 +1,52 @@
-<form id="login-form" action="/signup" method="POST">
+<script>
+  import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+  import { user$ } from "../store";
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+
+  const loginWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      user$.set(user);
+      localStorage.setItem("token", token);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+</script>
+
+<div>
+  {#if $user$}
+    <div>{$user$.displayName}로그인됨</div>
+  {/if}
   <div>로그인하기</div>
-  <div>
-    <lable for="id">아이디</lable>
-    <input type="text" id="id" name="id" required />
-  </div>
-  <div>
-    <lable for="password">패스워드</lable>
-    <input type="password" id="password" name="password" required />
-  </div>
-  <div>
-    <button type="submit">로그인</button>
-  </div>
-  <div id="info"></div>
-</form>
+  <button class="login-btn" on:click={loginWithGoogle}>
+    <img
+      class="google-img"
+      src="https://www.google.com/imgres?q=%EA%B5%AC%EA%B8%80%EB%A7%88%ED%81%AC&imgurl=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FHDY7T%2FbtrY2our4Rw%2FFw6bz0QroBUp1YxglkkwEK%2Fimg.webp&imgrefurl=https%3A%2F%2Fdesignlog.org%2F2513060&docid=9yNGC7pTXIRW_M&tbnid=0l_BJeH_qEpbzM&vet=12ahUKEwiO_tXShYOGAxVojVYBHQhhCYEQM3oECBoQAA..i&w=1000&h=563&hcb=2&ved=2ahUKEwiO_tXShYOGAxVojVYBHQhhCYEQM3oECBoQAA"
+      alt=""
+    />
+    <div>Google로 시작하기</div>
+    <div />
+  </button>
+</div>
+
+<style>
+  .login-btn {
+    width: 200px;
+    height: 50px;
+    border: 1px solid gray;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+    border-radius: 3px;
+  }
+
+  .google-img {
+    width: 20px;
+  }
+</style>
